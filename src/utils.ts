825add7097537d0ee8c8270fad6e4de546c7d07b
nyxb <contact@nyxb.xyz>
2023-05-21 12:53:08 +0200

    🎨 style(utils.ts): format code with consistent indentation and remove unnecessary semicolons 💄 refactor(utils.ts): simplify the isReadonlyArray function by removing the parentheses around the argument 💄 refactor(utils.ts): simplify the hasOwn function by removing the parentheses around the argument 💄 refactor(utils.ts): simplify the normalizeBoolean function by removing the unnecessary if statement 💄 refactor(utils.ts): simplify the applyParser function by removing the unnecessary if statement 💄 refactor(utils.ts): simplify the validateLuminarName function by removing the unnecessary parentheses around the argument 💄 refactor(utils.ts): simplify the createRegistry function by removing the unnecessary parentheses around the arguments 💄 refactor(utils.ts): simplify the finalizeLuminars function by removing the unnecessary parentheses around the arguments These changes improve the readability and maintainability of the code by removing unnecessary syntax and formatting the code with consistent indentation.

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,15 @@
 import type {
-	TypeFunction,
-	LuminarTypeOrSchema,
-	Luminars,
-} from './types';
+   LuminarTypeOrSchema,
+   Luminars,
+   TypeFunction,
+} from './types'
 
-const { stringify } = JSON;
-const camelCasePattern = /\B([A-Z])/g;
-const camelToKebab = (string: string) => string.replace(camelCasePattern, '-$1').toLowerCase();
+const { stringify } = JSON
+const camelCasePattern = /\B([A-Z])/g
+const camelToKebab = (string: string) => string.replace(camelCasePattern, '-$1').toLowerCase()
 
-const { hasOwnProperty } = Object.prototype;
-export const hasOwn = (object: any, property: PropertyKey) => hasOwnProperty.call(object, property);
+const { hasOwnProperty } = Object.prototype
+export const hasOwn = (object: any, property: PropertyKey) => hasOwnProperty.call(object, property)
 
 /**
  * Default Array.isArray doesn't support type-narrowing
@@ -17,162 +17,138 @@ export const hasOwn = (object: any, property: PropertyKey) => hasOwnProperty.cal
  *
  * https://stackoverflow.com/a/56249765/911407
  */
-const isReadonlyArray = (
-	array: readonly any[] | any,
-): array is readonly unknown[] => Array.isArray(array);
+function isReadonlyArray(array: readonly any[] | any): array is readonly unknown[] {
+   return Array.isArray(array)
+}
 
-export const parseLuminarType = (
-	luminarSchema: LuminarTypeOrSchema,
-): [parser: TypeFunction, isArray: boolean] => {
-	if (typeof luminarSchema === 'function') {
-		return [luminarSchema, false];
-	}
+export function parseLuminarType(luminarSchema: LuminarTypeOrSchema): [parser: TypeFunction, isArray: boolean] {
+   if (typeof luminarSchema === 'function')
+      return [luminarSchema, false]
 
-	if (isReadonlyArray(luminarSchema)) {
-		return [luminarSchema[0], true];
-	}
+   if (isReadonlyArray(luminarSchema))
+      return [luminarSchema[0], true]
 
-	return parseLuminarType(luminarSchema.type);
-};
+   return parseLuminarType(luminarSchema.type)
+}
 
-export const normalizeBoolean = <T>(
-	parser: TypeFunction,
-	value: T,
-) => {
-	if (parser === Boolean) {
-		return value !== 'false';
-	}
+export function normalizeBoolean<T>(parser: TypeFunction,
+   value: T) {
+   if (parser === Boolean)
+      return value !== 'false'
 
-	return value;
-};
+   return value
+}
 
-export const applyParser = (
-	typeFunction: TypeFunction,
-	value: any,
-) => {
-	if (typeof value === 'boolean') {
-		return value;
-	}
+export function applyParser(typeFunction: TypeFunction,
+   value: any) {
+   if (typeof value === 'boolean')
+      return value
 
-	if (typeFunction === Number && value === '') {
-		return Number.NaN;
-	}
+   if (typeFunction === Number && value === '')
+      return Number.NaN
 
-	return typeFunction(value);
-};
+   return typeFunction(value)
+}
 
-const reservedCharactersPattern = /[\s.:=]/;
+const reservedCharactersPattern = /[\s.:=]/
 
-const validateLuminarName = (
-	luminarName: string,
-) => {
-	const errorPrefix = `Luminar name ${stringify(luminarName)}`;
+function validateLuminarName(luminarName: string) {
+   const errorPrefix = `Luminar name ${stringify(luminarName)}`
 
-	if (luminarName.length === 0) {
-		throw new Error(`${errorPrefix} cannot be empty`);
-	}
+   if (luminarName.length === 0)
+      throw new Error(`${errorPrefix} cannot be empty`)
 
-	if (luminarName.length === 1) {
-		throw new Error(`${errorPrefix} must be longer than a character`);
-	}
+   if (luminarName.length === 1)
+      throw new Error(`${errorPrefix} must be longer than a character`)
 
-	const hasReservedCharacter = luminarName.match(reservedCharactersPattern);
-	if (hasReservedCharacter) {
-		throw new Error(`${errorPrefix} cannot contain ${stringify(hasReservedCharacter?.[0])}`);
-	}
-};
+   const hasReservedCharacter = luminarName.match(reservedCharactersPattern)
+   if (hasReservedCharacter)
+      throw new Error(`${errorPrefix} cannot contain ${stringify(hasReservedCharacter?.[0])}`)
+}
 
 type LuminarParsingData = [
-	values: unknown[],
-	parser: TypeFunction,
-	isArray: boolean,
-	schema: LuminarTypeOrSchema,
-];
+   values: unknown[],
+   parser: TypeFunction,
+   isArray: boolean,
+   schema: LuminarTypeOrSchema,
+]
 
-type LuminarRegistry = {
-	[luminarName: string]: LuminarParsingData;
-};
+interface LuminarRegistry {
+   [luminarName: string]: LuminarParsingData
+}
 
-export const createRegistry = (
-	schemas: Luminars,
-) => {
-	const registry: LuminarRegistry = {};
+export function createRegistry(schemas: Luminars) {
+   const registry: LuminarRegistry = {}
 
-	const setLuminar = (
-		luminarName: string,
-		data: LuminarParsingData,
-	) => {
-		if (hasOwn(registry, luminarName)) {
-			throw new Error(`Duplicate luminars named ${stringify(luminarName)}`);
-		}
+   const setLuminar = (
+      luminarName: string,
+      data: LuminarParsingData,
+   ) => {
+      if (hasOwn(registry, luminarName))
+         throw new Error(`Duplicate luminars named ${stringify(luminarName)}`)
 
-		registry[luminarName] = data;
-	};
+      registry[luminarName] = data
+   }
 
-	for (const luminarName in schemas) {
-		if (!hasOwn(schemas, luminarName)) {
-			continue;
-		}
-		validateLuminarName(luminarName);
+   for (const luminarName in schemas) {
+      if (!hasOwn(schemas, luminarName))
+         continue
 
-		const schema = schemas[luminarName];
-		const luminarData: LuminarParsingData = [
-			[],
-			...parseLuminarType(schema),
-			schema,
-		];
+      validateLuminarName(luminarName)
 
-		setLuminar(luminarName, luminarData);
+      const schema = schemas[luminarName]
+      const luminarData: LuminarParsingData = [
+         [],
+         ...parseLuminarType(schema),
+         schema,
+      ]
 
-		const kebabCasing = camelToKebab(luminarName);
-		if (luminarName !== kebabCasing) {
-			setLuminar(kebabCasing, luminarData);
-		}
+      setLuminar(luminarName, luminarData)
 
-		if ('alias' in schema && typeof schema.alias === 'string') {
-			const { alias } = schema;
-			const errorPrefix = `Luminar alias ${stringify(alias)} for luminar ${stringify(luminarName)}`;
+      const kebabCasing = camelToKebab(luminarName)
+      if (luminarName !== kebabCasing)
+         setLuminar(kebabCasing, luminarData)
 
-			if (alias.length === 0) {
-				throw new Error(`${errorPrefix} cannot be empty`);
-			}
+      if ('alias' in schema && typeof schema.alias === 'string') {
+         const { alias } = schema
+         const errorPrefix = `Luminar alias ${stringify(alias)} for luminar ${stringify(luminarName)}`
 
-			if (alias.length > 1) {
-				throw new Error(`${errorPrefix} must be a single character`);
-			}
+         if (alias.length === 0)
+            throw new Error(`${errorPrefix} cannot be empty`)
 
-			setLuminar(alias, luminarData);
-		}
-	}
+         if (alias.length > 1)
+            throw new Error(`${errorPrefix} must be a single character`)
 
-	return registry;
-};
+         setLuminar(alias, luminarData)
+      }
+   }
 
-export const finalizeLuminars = (
-	schemas: Luminars,
-	registry: LuminarRegistry,
-) => {
-	const luminars: Record<string, unknown> = {};
+   return registry
+}
 
-	for (const luminarName in schemas) {
-		if (!hasOwn(schemas, luminarName)) {
-			continue;
-		}
+export function finalizeLuminars(schemas: Luminars,
+   registry: LuminarRegistry) {
+   const luminars: Record<string, unknown> = {}
 
-		const [values, , isArray, schema] = registry[luminarName];
-		if (
-			values.length === 0
-			&& 'default' in schema
-		) {
-			let { default: defaultValue } = schema;
-			if (typeof defaultValue === 'function') {
-				defaultValue = defaultValue();
-			}
-			luminars[luminarName] = defaultValue;
-		} else {
-			luminars[luminarName] = isArray ? values : values.pop();
-		}
-	}
+   for (const luminarName in schemas) {
+      if (!hasOwn(schemas, luminarName))
+         continue
 
-	return luminars;
-};
+      const [values, , isArray, schema] = registry[luminarName]
+      if (
+         values.length === 0
+         && 'default' in schema
+      ) {
+         let { default: defaultValue } = schema
+         if (typeof defaultValue === 'function')
+            defaultValue = defaultValue()
+
+         luminars[luminarName] = defaultValue
+      }
+      else {
+         luminars[luminarName] = isArray ? values : values.pop()
+      }
+   }
+
+   return luminars
+}
