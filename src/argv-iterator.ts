39df4157818d7865788204b38281e9892e4eb0fb
nyxb <contact@nyxb.xyz>
2023-05-21 12:53:41 +0200

    🔨 refactor(argv-iterator.ts): remove unnecessary whitespace and fix type formatting This commit removes unnecessary whitespace and fixes the formatting of the Index type in the argv-iterator.ts file. No functionality has been changed.

--- src/argv-iterator.ts
+++ src/argv-iterator.ts
@@ -1,145 +1,134 @@
-export const DOUBLE_DASH = '--';
+export const DOUBLE_DASH = '--'
 
-export type Index =
-	| [index: number]
-	| [index: number, aliasIndex: number, isLast: boolean];
+export type Index = | [index: number] | [index: number, aliasIndex: number, isLast: boolean]
 
 type onValueCallbackType = (
-	value?: string,
-	index?: Index,
-) => void;
+   value?: string,
+   index?: Index,
+) => void
 
 type onLuminar = (
-	name: string,
-	value: string | undefined,
-	index: Index,
-) => void | onValueCallbackType;
+   name: string,
+   value: string | undefined,
+   index: Index,
+) => void | onValueCallbackType
 
 type onArgument = (
-	args: string[],
-	index: Index,
-	isEoF?: boolean,
-) => void;
+   args: string[],
+   index: Index,
+   isEoF?: boolean,
+) => void
 
-const valueDelimiterPattern = /[.:=]/;
+const valueDelimiterPattern = /[.:=]/
 
-const isLuminarPattern = /^-{1,2}\w/;
+const isLuminarPattern = /^-{1,2}\w/
 
-export const parseLuminarArgv = (
-	luminarArgv: string,
-): [
-	luminarName: string,
-	luminarValue: string | undefined,
-	isAlias: boolean,
-] | undefined => {
-	if (!isLuminarPattern.test(luminarArgv)) {
-		return;
-	}
+export function parseLuminarArgv(luminarArgv: string): [
+  luminarName: string,
+  luminarValue:string | undefined, isAlias: boolean,
+] | undefined {
+   if (!isLuminarPattern.test(luminarArgv))
+      return
 
-	const isAlias = !luminarArgv.startsWith(DOUBLE_DASH);
-	let luminarName = luminarArgv.slice(isAlias ? 1 : 2);
-	let luminarValue;
+   const isAlias = !luminarArgv.startsWith(DOUBLE_DASH)
+   let luminarName = luminarArgv.slice(isAlias ? 1 : 2)
+   let luminarValue
 
-	const hasValueDalimiter = luminarName.match(valueDelimiterPattern);
-	if (hasValueDalimiter) {
-		const { index } = hasValueDalimiter;
-		luminarValue = luminarName.slice(index! + 1);
-		luminarName = luminarName.slice(0, index);
-	}
+   const hasValueDalimiter = luminarName.match(valueDelimiterPattern)
+   if (hasValueDalimiter) {
+      const { index } = hasValueDalimiter
+      luminarValue = luminarName.slice(index! + 1)
+      luminarName = luminarName.slice(0, index)
+   }
 
-	return [luminarName, luminarValue, isAlias];
-};
+   return [luminarName, luminarValue, isAlias]
+}
 
-export const argvIterator = (
-	argv: string[],
-	{
-		onLuminar,
-		onArgument,
-	}: {
-			onLuminar?: onLuminar;
-		onArgument?: onArgument;
-	},
-) => {
-	let onValueCallback: void | onValueCallbackType;
-	const triggerValueCallback = (
-		value?: string,
-		index?: Index,
-	) => {
-		if (typeof onValueCallback !== 'function') {
-			return true;
-		}
+export function argvIterator(argv: string[],
+   {
+      onLuminar,
+      onArgument,
+   }: {
+      onLuminar?: onLuminar
+      onArgument?: onArgument
+   }) {
+   let onValueCallback: void | onValueCallbackType
+   const triggerValueCallback = (
+      value?: string,
+      index?: Index,
+   ) => {
+      if (typeof onValueCallback !== 'function')
+         return true
 
-		onValueCallback(value, index);
-		onValueCallback = undefined;
-	};
+      onValueCallback(value, index)
+      onValueCallback = undefined
+   }
 
-	for (let i = 0; i < argv.length; i += 1) {
-		const argvElement = argv[i];
+   for (let i = 0; i < argv.length; i += 1) {
+      const argvElement = argv[i]
 
-		if (argvElement === DOUBLE_DASH) {
-			triggerValueCallback();
+      if (argvElement === DOUBLE_DASH) {
+         triggerValueCallback()
 
-			const remaining = argv.slice(i + 1);
-			onArgument?.(remaining, [i], true);
-			break;
-		}
+         const remaining = argv.slice(i + 1)
+         onArgument?.(remaining, [i], true)
+         break
+      }
 
-		const parsedLuminar = parseLuminarArgv(argvElement);
+      const parsedLuminar = parseLuminarArgv(argvElement)
 
-		if (parsedLuminar) {
-			triggerValueCallback();
+      if (parsedLuminar) {
+         triggerValueCallback()
 
-			if (!onLuminar) {
-				continue;
-			}
+         if (!onLuminar)
+            continue
 
-			const [luminarName, luminarValue, isAlias] = parsedLuminar;
+         const [luminarName, luminarValue, isAlias] = parsedLuminar
 
-			if (isAlias) {
-				// Alias group
-				for (let j = 0; j < luminarName.length; j += 1) {
-					triggerValueCallback();
+         if (isAlias) {
+            // Alias group
+            for (let j = 0; j < luminarName.length; j += 1) {
+               triggerValueCallback()
 
-					const isLastAlias = j === luminarName.length - 1;
-					onValueCallback = onLuminar(
-						luminarName[j],
-						isLastAlias ? luminarValue : undefined,
-						[i, j + 1, isLastAlias],
-					);
-				}
-			} else {
-				onValueCallback = onLuminar(
-					luminarName,
-					luminarValue,
-					[i],
-				);
-			}
-		} else if (triggerValueCallback(argvElement, [i])) { // if no callback was set
-			onArgument?.([argvElement], [i]);
-		}
-	}
+               const isLastAlias = j === luminarName.length - 1
+               onValueCallback = onLuminar(
+                  luminarName[j],
+                  isLastAlias ? luminarValue : undefined,
+                  [i, j + 1, isLastAlias],
+               )
+            }
+         }
+         else {
+            onValueCallback = onLuminar(
+               luminarName,
+               luminarValue,
+               [i],
+            )
+         }
+      }
+      else if (triggerValueCallback(argvElement, [i])) { // if no callback was set
+         onArgument?.([argvElement], [i])
+      }
+   }
 
-	triggerValueCallback();
-};
+   triggerValueCallback()
+}
 
-export const spliceFromArgv = (
-	argv: string[],
-	removeArgvs: Index[],
-) => {
-	for (const [index, aliasIndex, isLast] of removeArgvs.reverse()) {
-		if (aliasIndex) {
-			const element = argv[index];
-			let newValue = element.slice(0, aliasIndex);
-			if (!isLast) {
-				newValue += element.slice(aliasIndex + 1);
-			}
+export function spliceFromArgv(argv: string[],
+   removeArgvs: Index[]) {
+   for (const [index, aliasIndex, isLast] of removeArgvs.reverse()) {
+      if (aliasIndex) {
+         const element = argv[index]
+         let newValue = element.slice(0, aliasIndex)
+         if (!isLast)
+            newValue += element.slice(aliasIndex + 1)
 
-			if (newValue !== '-') {
-				argv[index] = newValue;
-				continue;
-			}
-		}
+         if (newValue !== '-') {
+            argv[index] = newValue
+            continue
+         }
+      }
 
-		argv.splice(index, 1);
-	}
-};
+      argv.splice(index, 1)
+   }
+}
