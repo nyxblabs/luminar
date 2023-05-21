d845fa3c3188d295934845f07a040d62d3f92ddd
nyxb <contact@nyxb.xyz>
2023-05-21 12:55:16 +0200

    🔨 refactor(luminar.ts): improve code readability by using consistent indentation and whitespace The changes in this commit are purely cosmetic and do not affect the functionality of the code. The code has been reformatted to use consistent indentation and whitespace to improve readability.

--- src/luminar.ts
+++ src/luminar.ts
@@ -1,27 +1,27 @@
 import type {
-	Luminars,
-	ParsedLuminars,
-	TypeLuminar,
-	TypeLuminarOptions,
-} from './types';
+   Luminars,
+   ParsedLuminars,
+   TypeLuminar,
+   TypeLuminarOptions,
+} from './types'
 import {
-	KNOWN_LUMINAR,
-	UNKNOWN_LUMINAR,
-	ARGUMENT,
-} from './types';
+   ARGUMENT,
+   KNOWN_LUMINAR,
+   UNKNOWN_LUMINAR,
+} from './types'
 import {
-	hasOwn,
-	createRegistry,
-	normalizeBoolean,
-	applyParser,
-	finalizeLuminars,
-} from './utils';
+   applyParser,
+   createRegistry,
+   finalizeLuminars,
+   hasOwn,
+   normalizeBoolean,
+} from './utils'
 import {
-	DOUBLE_DASH,
-	argvIterator,
-	spliceFromArgv,
-	type Index,
-} from './argv-iterator';
+   DOUBLE_DASH,
+   type Index,
+   argvIterator,
+   spliceFromArgv,
+} from './argv-iterator'
 
 /**
 luminar: typed argv parser
@@ -35,98 +35,93 @@ luminar: typed argv parser
 import typeLuminar from 'luminar';
 
 const parsed = typeLuminar({
-	foo: Boolean,
-	bar: {
-		type: Number,
-		default: 8
-	}
+   foo: Boolean,
+   bar: {
+   type: Number,
+   default: 8
+   }
 })
 ```
 */
-export const typeLuminar = <Schemas extends Luminars>(
-	schemas: Schemas,
-	argv: string[] = process.argv.slice(2),
-	{ ignore }: TypeLuminarOptions = {},
-) => {
-	const removeArgvs: Index[] = [];
-	const luminarRegistry = createRegistry(schemas);
-	const unknownLuminars: ParsedLuminars['unknownLuminars'] = {};
-	const _ = [] as unknown as ParsedLuminars['_'];
-	_[DOUBLE_DASH] = [];
+export function typeLuminar<Schemas extends Luminars>(schemas: Schemas,
+   argv: string[] = process.argv.slice(2),
+   { ignore }: TypeLuminarOptions = {}) {
+   const removeArgvs: Index[] = []
+   const luminarRegistry = createRegistry(schemas)
+   const unknownLuminars: ParsedLuminars['unknownLuminars'] = {}
+   const _ = [] as unknown as ParsedLuminars['_']
+   _[DOUBLE_DASH] = []
 
-	argvIterator(argv, {
-		onLuminar(name, explicitValue, luminarIndex) {
-			const isKnownLuminar = hasOwn(luminarRegistry, name);
-			if (
-				ignore?.(
-					isKnownLuminar ? KNOWN_LUMINAR : UNKNOWN_LUMINAR,
-					name,
-					explicitValue,
-				)
-			) {
-				return;
-			}
+   argvIterator(argv, {
+      onLuminar(name, explicitValue, luminarIndex) {
+         const isKnownLuminar = hasOwn(luminarRegistry, name)
+         if (
+            ignore?.(
+               isKnownLuminar ? KNOWN_LUMINAR : UNKNOWN_LUMINAR,
+               name,
+               explicitValue,
+            )
+         )
+            return
 
-			if (isKnownLuminar) {
-				const [values, parser] = luminarRegistry[name];
-				const luminarValue = normalizeBoolean(parser, explicitValue);
-				const getFollowingValue = (
-					value?: string | boolean,
-					valueIndex?: Index,
-				) => {
-					// Remove elements from argv array
-					removeArgvs.push(luminarIndex);
-					if (valueIndex) {
-						removeArgvs.push(valueIndex);
-					}
+         if (isKnownLuminar) {
+            const [values, parser] = luminarRegistry[name]
+            const luminarValue = normalizeBoolean(parser, explicitValue)
+            const getFollowingValue = (
+               value?: string | boolean,
+               valueIndex?: Index,
+            ) => {
+               // Remove elements from argv array
+               removeArgvs.push(luminarIndex)
+               if (valueIndex)
+                  removeArgvs.push(valueIndex)
 
-					values.push(
-						applyParser(parser, value || ''),
-					);
-				};
+               values.push(
+                  applyParser(parser, value || ''),
+               )
+            }
 
-				return (
-					luminarValue === undefined
-						? getFollowingValue
-						: getFollowingValue(luminarValue)
-				);
-			}
+            return (
+               luminarValue === undefined
+                  ? getFollowingValue
+                  : getFollowingValue(luminarValue)
+            )
+         }
 
-			if (!hasOwn(unknownLuminars, name)) {
-				unknownLuminars[name] = [];
-			}
+         if (!hasOwn(unknownLuminars, name))
+            unknownLuminars[name] = []
 
-			unknownLuminars[name].push(
-				explicitValue === undefined ? true : explicitValue,
-			);
-			removeArgvs.push(luminarIndex);
-		},
+         unknownLuminars[name].push(
+            explicitValue === undefined ? true : explicitValue,
+         )
+         removeArgvs.push(luminarIndex)
+      },
 
-		onArgument(args, index, isEoF) {
-			if (ignore?.(ARGUMENT, argv[index[0]])) {
-				return;
-			}
+      onArgument(args, index, isEoF) {
+         if (ignore?.(ARGUMENT, argv[index[0]]))
+            return
 
-			_.push(...args);
+         _.push(...args)
 
-			if (isEoF) {
-				_[DOUBLE_DASH] = args;
-				argv.splice(index[0]);
-			} else {
-				removeArgvs.push(index);
-			}
-		},
-	});
+         if (isEoF) {
+            _[DOUBLE_DASH] = args
+            argv.splice(index[0])
+         }
+         else {
+            removeArgvs.push(index)
+         }
+      },
+   })
 
-	spliceFromArgv(argv, removeArgvs);
+   spliceFromArgv(argv, removeArgvs)
 
-	type Result = TypeLuminar<Schemas>;
-	return {
-		luminars: finalizeLuminars(schemas, luminarRegistry),
-		unknownLuminars,
-		_,
-	} as {
-		// This exposes the content of "TypeLuminar<T>" in type hints
-		[Key in keyof Result]: Result[Key];
-	};
-};
+type Result = TypeLuminar<Schemas>
+return {
+   luminars: finalizeLuminars(schemas, luminarRegistry),
+   unknownLuminars,
+   _,
+} as {
+   // This exposes the content of "TypeLuminar<T>" in type hints
+   [Key in keyof Result]: Result[Key];
+}
+}
